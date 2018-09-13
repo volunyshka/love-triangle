8b2f1cea8691022f10509dec83a73366adeeba29
Volynets.olya <volynec.olya@gmail.com>
2018-09-13 23:45:12 +0300

    implement loveßtriangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var x = 0;
+  for (var i = 1 ; i < preferences.length; i++) {
+    var a;
+    var b;
+    var c;
+    a = preferences[i-1];
+    b = preferences[a-1];
+    c  = preferences [b-1];
+    if (c === i && i< a && i<b) {
+      x++
+
+      
+
+    }
+
+
+  } return x;
 };
+
+//getLoveTrianglesCount([2,3,1]);
